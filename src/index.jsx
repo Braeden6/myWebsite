c4734de16aa23fc95161f4d27638f735a8056210
Braeden <braeden.norman6@gmail.com>
2022-11-13 16:09:50 -0800

    BUG: trying to fix env variables

--- src/index.jsx
+++ src/index.jsx
@@ -8,11 +8,8 @@ import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./configFiles/authConfig"
 import './index.css';
 
-if (process.env.NODE_ENV !== 'production') {
-  console.log(import.meta.env.VITE_TEST);
-} else {
-  console.log(process.env.SOME_TEST_123);
-}
+
+console.log(import.meta.env.VITE_TEST);
 
 
 // disable console when in production
